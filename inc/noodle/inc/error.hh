999f4348c2fc54f45186a07bb304824ff15dd884
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-09-08 01:39:07 +0100

    added verbose error ctx based on enums

--- inc/noodle/inc/error.hh
+++ inc/noodle/inc/error.hh
@@ -34,6 +34,51 @@ namespace noodle
         // PRESUPPOSE A GENERIC RETURN ARG
         template<typename T, typename ...>
         struct IS_FORMATTABLE : std::false_type {};
+
+        // FORMATTABLE TYPE DETECTION
+        // BY QUERYING THE TYPE OF AN EXPRESSION BASED ON THE VALUE PROVIDED
+        // GREAT FOR HAVING A DETERMINISTIC RETURN TYPE
+        template<typename T>
+        struct IS_FORMATTABLE<T, std::void_t<decltype(fmt::format("{}", std::declval<T>()))>> : std::true_type {};
+
+        // FORMATTABLE TYPE DETECTION
+        // BASED ON A BOOLEAN EXPRESSION
+        template<typename T>
+        constexpr bool IS_FORMATTABLE_V = IS_FORMATTABLE<T>::value;
+
+        enum class ERROR_CATEGORY : U8
+        {
+            LOGIC_ERR = 0,
+            RUNTIME_ERR,
+            RES_ERR,
+            INVALID_ARG,
+            OOB,
+            NULL_PTR,
+            UNIMPL,
+            UNREACH,
+            SYS_ERR,
+            CUSTOM_ERR
+        };
+
+        enum class ERROR_SEVERITY : U8
+        {
+            FATAL = 0,
+            CRITICAL,
+            STD_ERROR,
+            WARNING,
+            INFO
+        };
+
+        // ERROR CONTEXT HANDLER WHICH PRESUPPOSES THE GENERIC TYPES
+        // ASSOCIATED WITH AN ERROR TYPE
+        template<typename CODE_TYPE = int, typename MSG_TYPE = std::string, typename ...>
+        struct ERROR_CTX
+        {
+            using ERROR_CODE = CODE_TYPE;
+            using ERROR_MSG = MSG_TYPE;
+            ERROR_CATEGORY CATEGORY;
+            ERROR_SEVERITY SEVERITY;
+        };
     }
 }
 
