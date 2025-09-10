9bca06e7d36cf47ca74a2f8ba9024f4359ccbc5d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-09-10 01:13:24 +0100

    added same comparators and constexprs from siena

--- inc/noodle/memory.hh
+++ inc/noodle/memory.hh
@@ -31,6 +31,29 @@ namespace fujiko
         using FUJIKO_READ_8     = std::function<U8(U32 ADDRESS, void* CTX)>;
         using FUJIKO_READ_16    = std::function<U16(U32 ADDRESS, void* CTX)>;
         using FUJIKO_READ_32    = std::function<U32(U32 ADDRESS, void* CTX)>;
+
+        using FUJIKO_WRITE_8    = std::function<void(U32, U8, void*)>;
+        using FUJIKO_WRITE_16   = std::function<void(U32, U16, void*)>;
+        using FUJIKO_WRITE_32   = std::function<void(U32, U32, void*)>;
+
+        // GENERIC CONVERTIBLE CLAUSE FOR BEING ABLE TO CONVERT BETWEEN TWO GENERICS
+        // RETURNS: AN ASSIGNED INTEGRAL CONSTANT
+
+        template <typename T1, typename T2>
+        static inline constexpr bool IS_ASSIGNABLE = std::is_convertible<T1, T2>::value;
+
+        // THE FOLLOWING SERVES TO PROVIDE A TYPE-SAFE MEANS OF BEING ABLE TO ASSIGN
+        // RELEVANT SIZES, AND ARGUMENTS IN RELATION TO THE SIZE OF THE MEMORY BUS
+        
+        // WHICH PRESUPPOSES THE ARGS PROVIDED THROUGH A GENERIC
+        template<typename T>
+        static inline constexpr bool FUJIKO_BUS_HANDLER = IS_ASSIGNABLE<FUJIKO_READ_8, T> 
+                                                        || IS_ASSIGNABLE<FUJIKO_READ_16, T>
+                                                        || IS_ASSIGNABLE<FUJIKO_READ_32, T>
+                                                        || IS_ASSIGNABLE<FUJIKO_WRITE_8, T>
+                                                        || IS_ASSIGNABLE<FUJIKO_WRITE_16, T>
+                                                        || IS_ASSIGNABLE<FUJIKO_WRITE_32, T>;
+
     }
 }
 
